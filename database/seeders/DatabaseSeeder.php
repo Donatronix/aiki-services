f1a9628b443731585267ad87623711c3baffacd1
Donald Blessing <donaldblessing9@gmail.com>
2021-04-04 14:15:52 +0100

    Reordered table seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,8 +17,8 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
-            AdminTableSeeder::class,
             RolesTableSeeder::class,
+            AdminTableSeeder::class,
         ]);
     }
 }
